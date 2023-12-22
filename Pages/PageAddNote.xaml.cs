2f3fe2e4280aecd79cdf147deecebc5ed5332aab
Александр Кулаков <godiwni@yandex.ru>
2023-12-22 19:50:10 +0300

    Добавьте файлы проекта.

---
+++ Pages/PageAddNote.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LB2.Classes;
+
+namespace LB2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageAddNote.xaml
+    /// </summary>
+    public partial class PageAddNote : Page
+    {
+        private NOTE _currentNote = new NOTE();
+        public PageAddNote(NOTE selectedNote)
+        {
+            InitializeComponent();
+            DataContext = _currentNote;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder error = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(_currentNote.FirstName)) error.AppendLine("Укажите имя");
+            if (string.IsNullOrWhiteSpace(_currentNote.LastName)) error.AppendLine("Укажите фамилию");
+            if (string.IsNullOrWhiteSpace(_currentNote.PhoneNumber)) error.AppendLine("Укажите номер телефона");
+            if (string.IsNullOrWhiteSpace(Convert.ToString(_currentNote.Birthday))) error.AppendLine("Укажите дату рождения");
+            if (error.Length > 0)
+            {
+                MessageBox.Show(error.ToString());
+                return;
+            }
+            if (_currentNote.idNote == 0)
+            {
+                LB2Entities.GetContext().NOTE.Add(_currentNote);
+                try
+                {
+                    LB2Entities.GetContext().SaveChanges();
+                    Classes.ClassFrame.frmObj.Navigate(new PageNote());
+                    MessageBox.Show("Новый человек успешно добавлен!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+            {
+                try
+                {
+                    LB2Entities.GetContext().SaveChanges();
+                    Classes.ClassFrame.frmObj.Navigate(new PageNote());
+                    MessageBox.Show("Человек успешно изменён!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frmObj.Navigate(new PageNote());
+        }
+    }
+}
---
+++ Pages/PageNote.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LB2.Classes;
+
+namespace LB2.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageNote.xaml
+    /// </summary>
+    public partial class PageNote : Page
+    {
+        public PageNote()
+        {
+            InitializeComponent();
+            DtgNote.ItemsSource = LB2Entities.GetContext().NOTE.ToList();
+        }
+
+        private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string search = TxbSearch.Text;
+            var listSomeValues = LB2Entities.GetContext().NOTE.ToList();
+            if (!string.IsNullOrWhiteSpace(search))
+                DtgNote.ItemsSource = listSomeValues.
+                Where(x => x.Birthday.Value.ToString("MMMM").ToLower().Contains(search.ToLower())).ToList();
+            else
+                DtgNote.ItemsSource = LB2Entities.GetContext().NOTE.ToList();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            ClassFrame.frmObj.Navigate(new PageAddNote(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var usersForRemoving = DtgNote.SelectedItems.Cast<NOTE>().ToList();
+            if (MessageBox.Show($"Удалить {usersForRemoving.Count()} запись?", "Внимание!",
+                MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                try
+                {
+                    LB2Entities.GetContext().NOTE.RemoveRange(usersForRemoving);
+                    LB2Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            DtgNote.ItemsSource = LB2Entities.GetContext().NOTE.ToList();
+        }
+    }
+}
